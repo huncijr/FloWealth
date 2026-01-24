378948cb8eb9845ef0eed0bcc56f69265005528a
huncijr <hunor.burian08@gmail.com>
2026-01-24 10:27:35 +0200

    first commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Button>Click me!</Button>
+      <div className="text-blue-400">aaa</div>
+    </>
+  );
+}
+
+export default App;
