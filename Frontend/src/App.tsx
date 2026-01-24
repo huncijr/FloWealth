b21bb069af78e87bc3ac8ac2ac0788a2a0eab310
huncijr <hunor.burian08@gmail.com>
2026-01-24 12:18:12 +0200

    Added Backend with Typescript and axios in Frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,7 @@ import { Button } from "@heroui/react";
 import "./index.css";
 
 function App() {
-  return (
-    <>
-      <Button>Click me!</Button>
-      <div className="text-blue-400">aaa</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ Frontend/src/api/axiosInstance.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance } from "axios";
+
+const baseURL: string = import.meta.env.PROD
+  ? (import.meta.env.VITE_API_URL as string)
+  : "http://localhost:5001";
+
+export const api: AxiosInstance = axios.create({
+  baseURL,
+  timeout: 8000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    if (status === 404) {
+      console.error("NOT FOUND ERROR");
+    }
+    if (status === 500) {
+      console.log("SERVER ERROR");
+    }
+    return Promise.reject(error);
+  },
+);
